f7469a1e3d6df548b1b4ac3c7a82302eae60fc26
exxon suarez <suarez.exxon@gmail.com>
2023-05-09 14:38:27 -0700

    Lab09.1 complete

---
+++ Lab09.1/src/com/hr/personnel/SalariedEmployee.java
@@ -0,0 +1,62 @@
+package com.hr.personnel;
+
+import gov.irs.TaxPayer;
+
+import java.time.LocalDate;
+
+public class SalariedEmployee extends Employee  {
+    // instance variables (fields)
+    private double salary;
+
+
+
+
+
+    // constructors
+    public SalariedEmployee() {
+
+    }
+
+    public SalariedEmployee(String name, LocalDate hireDate) {
+        super(name, hireDate);
+        // enrollIn401k()
+    }
+
+
+    public SalariedEmployee(String name, LocalDate hireDate, double salary) {
+        this(name, hireDate);  // delegate to neighboring ctor above me for name , hireDate
+        setSalary(salary);  // delegate to setter for validation or conversion
+    }
+
+    // business methods or "action" methods
+    @Override
+    public void pay() {
+        System.out.println(getName() + " is paid salary " + getSalary());       // inherited getName() method from base class Employee
+    }
+
+    public void takeVacation() {
+        System.out.println( getName() + " is on Vacation...");
+    }
+
+    // accessors methods
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+
+    // toString method
+    @Override
+    public String toString() {
+        return "SalariedEmployee: name= " + super.toString() + ", salary= " + getSalary();      // using super.toString to retrieve fields already returned in the Employee base class
+    }
+
+    @Override
+    public void payTaxes() {
+        System.out.println( getName() + "Payed taxes of: " + TaxPayer.SALARIED_TAX_RATE * getSalary());
+    }
+}
