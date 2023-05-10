8d8ecb24b6f510a0d4c69f47ea7f44ddbf8af5b3
exxon suarez <suarez.exxon@gmail.com>
2023-05-10 11:13:33 -0700

    added test method for throws exception

---
+++ Lab10.1-Television/src/com/entertainment/client/TelevisionClient.java
@@ -0,0 +1,11 @@
+package com.entertainment.client;
+
+import com.entertainment.Television;
+
+class TelevisionClient {
+    public static void main(String[] args) {
+        Television tv1 = new Television("Sony", 50);
+
+        System.out.println(tv1);
+    }
+}
