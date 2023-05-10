86ac8e2099776ac0519c364057d1c99a78a08bee
exxon suarez <suarez.exxon@gmail.com>
2023-05-10 15:23:24 -0700

    Lab10.1 Checked Exceptions

---
+++ Lab10.1-HR/src/gov/irs/IllegalWageException.java
@@ -0,0 +1,26 @@
+package gov.irs;
+
+public class IllegalWageException extends Exception {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public IllegalWageException() {
+    }
+
+    public IllegalWageException(String message) {
+        super(message);
+    }
+
+    public IllegalWageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IllegalWageException(Throwable cause) {
+        super(cause);
+    }
+
+
+}
